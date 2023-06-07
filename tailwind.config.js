752ed82ab9ca7954d9d7f16c21b8ccdf3b379929
Mary Therese Meldionne Caseria <caseria.therese@gmail.com>
2023-06-08 01:32:09 +0800

    #7 color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#004275',
+        'accent2': '#FDAF77'
+      },
+    },
   },
   plugins: [],
 }
